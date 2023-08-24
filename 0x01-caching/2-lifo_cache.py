bdc8fe21c3fc78eade436a9e71d0a2b2c183f4bf
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-08-24 04:52:31 +0000

    reimplemented lifo queue with lists

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -2,7 +2,6 @@
 """
 BasicCache Module
 """
-from queue import LifoQueue
 
 BaseCaching = __import__('base_caching').BaseCaching
 
@@ -12,25 +11,29 @@ class LIFOCache(BaseCaching):
     LIFO based replacement policy
     """
     def __init__(self):
-        """Initialize with super class
+        """
+        Initialize with super class.
+        Initialize super class and new lifo queu
         """
         super().__init__()
-        self._lifo = LifoQueue()
+        self._lifo = []
 
     def put(self, key, item):
-        """Put the new item in cache
+        """Put the new item in cache using lifo
+        replacement policy
         """
         if key is None or item is None:
             pass
         else:
             self.cache_data[key] = item
             if len(self.cache_data) > self.MAX_ITEMS:
-                a = self._lifo.get()
+                a = self._lifo.pop(len(self._lifo) - 1)
                 print("DISCARD: {}".format(a))
                 del self.cache_data[a]
-            self._lifo.put(key)
+            self._lifo.append(key)
 
     def get(self, key):
         """Get item from cache if there
+        it returns a match or None
         """
         return self.cache_data.get(key, None)
