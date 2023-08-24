366cb8da8f9f935bbe637a0aa718653af5940a95
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-08-24 02:31:44 +0000

    added LIFO Cache. Have to remove iitem from cache before adding one --

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+BasicCache Module
+"""
+from queue import LifoQueue
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Basic cache based on basecaching class.
+    """
+    def __init__(self):
+        """Initialize with super class
+        """
+        super().__init__()
+        self._lifo = LifoQueue()
+
+    def put(self, key, item):
+        """Put the new item in cache
+        """
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+            if len(self.cache_data) > self.MAX_ITEMS:
+                a = self._lifo.get()
+                print("DISCARD: {}".format(a))
+                del self.cache_data[a]
+            self._lifo.put(key)
+    
+    def get(self, key):
+        """Get item from cache if there
+        """
+        return self.cache_data.get(key, None)
