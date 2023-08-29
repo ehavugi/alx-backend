642367bf5c8def3bbdf90b514264516ab8a3333b
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-08-29 06:56:26 +0000

    Added test for style and fixed style

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 """
+Main app page
 """
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
     """
+    App route. Main page
     """
     title = "Welcome to Holberton"
     header = "Hello world"
     return render_template("0-index.html", title=title, header=header)
 
-if __name__ == '__main__': 
-   app.run(port=80)
+
+if __name__ == '__main__':
+    app.run(port=80)
