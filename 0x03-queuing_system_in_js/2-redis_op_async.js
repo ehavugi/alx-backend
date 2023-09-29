8078f2f8f24aa90d6c39e72fbcd6b460cacbec12
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-09-29 03:07:45 +0000

    added 2-redis_op_async :

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,33 @@
+import redis from 'redis';
+import { promisify } from 'util'; // Import the 'promisify' function
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Promisify the 'set' and 'get' functions
+const getAsync = promisify(client.get).bind(client);
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  try {
+    const reply = await getAsync(schoolName);
+    console.log(`${reply}`);
+  } catch (err) {
+    throw err;
+ } 
+}
+
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
